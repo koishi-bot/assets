94505ca43e6714b1f8b3ef50882ad7615555f99a
shigma <shigma@users.noreply.github.com>
2024-02-23 18:19:13 +0000

    [skip ci] deploy koishijs/assets@9ec6968612f314aa06598b2969ff2dc342ce0280

---
+++ assets/zh-CN_index.md.B3Fj6q-i.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,l as i,a,a6 as t}from"./chunks/framework.Byih1hIo.js";const d=JSON.parse('{"title":"介绍","description":"","frontmatter":{},"headers":[],"relativePath":"zh-CN/index.md","filePath":"zh-CN/index.md"}'),l={name:"zh-CN/index.md"},s=t("",9),r=[s];function h(o,n,p,u,_,c){return a(),i("div",null,r)}const f=e(l,[["render",h]]);export{d as __pageData,f as default};
